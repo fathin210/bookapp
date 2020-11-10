4edbf6fd4eb0572e929524d1627f67e283eaacc9
fathin210 <57560847+fathin210@users.noreply.github.com>
2020-11-10 14:28:59 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,12 @@ $router->get('/key', function () {
 
 $router->get('/books', 'BooksController@index');
 $router->get('/books/{id}', 'BooksController@show');
+$router->post('/books', 'BooksController@store');
+$router->put('/books/{id}', 'BooksController@update');
+$router->delete('/books/{id}', 'BooksController@destroy');
+
+$router->get('/authors', 'AuthorsController@index');
+$router->get('/authors/{id}', 'AuthorsController@show');
+$router->post('/authors', 'AuthorsController@store');
+$router->put('/authors/{id}', 'AuthorsController@update');
+$router->delete('/authors/{id}', 'AuthorsController@destroy');
